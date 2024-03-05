c256c759bd4ed06f480b4634d6256c11fddc526f
YifanZhang4 <bookworm.with.a.problem@gmail.com>
2024-03-04 21:48:02 -0500

    server should be set up

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require("express");
 const port = process.env.PORT || 3000;
 const app = express();
 
-app.listen(port, ()=>{
-    console.log('Server is up!')
-})
+app.listen(port, () => {
+  console.log("Server is up!");
+});
