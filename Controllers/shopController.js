520c1b4851d3bb29918890677a169f4334a02844
Firdavsy07 <113189550+Firdavsy07@users.noreply.github.com>
2024-03-06 23:45:33 -0500

    saving models
    dekcs should be able to be created and named

--- Controllers/shopController.js
+++ Controllers/shopController.js
@@ -1,8 +1,18 @@
+const Deck = require("../Models/shops");
 exports.homePage = async (req, res) => {
   const decks = ["Whalen's Deck", "Meta Deck"];
   try {
-    res.send(req.query);
+    console.log(req.name);
     return res.send("We're live");
   } catch (error) {}
   console.log(error);
+};
+
+exports.createShop = async (res, req) => {
+  try {
+    const deck = new Deck(req.body);
+    await deck.save();
+  } catch (error) {
+    res.status(500).json(error);
+  }
 };
--- Routes/index.js
+++ Routes/index.js
@@ -2,5 +2,5 @@ const express = require("express");
 const router = new express.Router();
 const shopController = require("../Controllers/shopController");
 router.get("/", shopController.homePage);
-
+router.post("/add", shopController.createShop);
 module.exports = router;
