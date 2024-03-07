13d834846810b1a4751979706045f759d929a670
Firdavsy07 <113189550+Firdavsy07@users.noreply.github.com>
2024-03-06 23:25:12 -0500

    Update shops.js

--- Models/shops.js
+++ Models/shops.js
@@ -1,24 +1,17 @@
 const mongoose = require("mongoose");
 const slugify = require("slugify");
-const shopSchema = new mongoose.Schema({
+const deckSchema = new mongoose.Schema({
   name: {
     type: String,
     trim: true,
-    required: "Please create a Deck nName",
+    required: "Please create a Deck Name",
   },
   slug: String,
   description: {
     type: String,
     trim: true,
   },
-  tags: [String],
 });
 
-shopSchema.pre("save", function (next) {
-  if (!this.isModified(`name`));
-  {
-    next();
-    return;
-  }
-});
+
 mongoose.model.exports = mongose.model("Deck", deckSchema);
